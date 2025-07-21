9c7b27d86a4392aaf1f84e551f08fe3162792e89
jasmin rustemovic <rustemovic@gmail.com>
2025-07-21 22:15:38 +0200

    Add project files.

---
+++ Dispatcher.Bussiness/Mapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Dispatcher.DataAccess;
+using Dispatcher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispatcher.Bussiness.Mapper
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
---
+++ Dispatcher.Models/CategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispatcher.Models
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Morate uneti naziv kategorije")]
+        [MaxLength(50,ErrorMessage ="Maksimalna duzina za naziv kategorije je 50 karaktera")]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime Added_Date { get; set; } = DateTime.Now;
+    }
+}
